ce24601a6772fd7c4c1d3d2988dd624da5bccf26
Jimmy Bowens <jimmy@jimmybowens.com>
2014-11-06 11:22:50 -0500

    Got scanner to work the way I want it to

--- Editor.java
+++ Editor.java
@@ -1,26 +1,43 @@
+package editor;
+
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class Editor{
-  LinkedList data = new LinkedList();
-  public Editor(){
-    askForText();
-  }//end of editor constructor
-  
-  public void askForText(){
-    Scanner getInput = new Scanner(System.in);     //call scanner
-    
-    while(getInput.hasNext()){
-      data.add(getInput.nextLine());
-      //System.out.println(data);
-      if(getInput.hasNext("$")){
-        System.out.println(data);
-      }
+public class Editor {
+ public static boolean run = true;
+ @SuppressWarnings("rawtypes")
+ public static LinkedList data = new LinkedList();
+ public Editor(){
+  getInput();
+ }
+ 
+ @SuppressWarnings("rawtypes")
+ public void insert(Scanner input, LinkedList data){
+  System.out.println("TODO");
+ }
+ 
+ public void getInput(){
+  Scanner getTextInput = new Scanner(System.in);
+  boolean hasCommand = false;
+  while(getTextInput.hasNext()){
+   if(getTextInput.next().equalsIgnoreCase("$")){
+    while(hasCommand != true){
+     if(getTextInput.next().equalsIgnoreCase("insert")){
+      insert(getTextInput,data);
+      hasCommand = true;
+      getInput();
+     }
     }
-    
+   }
+   System.out.println("Please enter a '$' followed by a command.");
+   getInput();
   }
   
-  public static void main(String args[]) {
-    new Editor();
-  }//end of main
+ }
+
+ public static void main(String[] args) {
+  System.out.println("Welcome to the Editor");
+  new Editor();
+ }
+
 }
